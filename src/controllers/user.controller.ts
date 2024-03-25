48c25daaa1367ed673931e31e9a9d58565504ae3
trieu <lequoctrieu618@gmail.com>
2024-03-25 13:40:25 +0700

    add crud for admin user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,27 @@
 
 import express,{Request,Response} from 'express';
 import db from '../models';
+interface UserRequest extends Request{
+    body:{
+        id:number;
+        name:string;
+        email:string;
+        password:string;
+        serviceId:number;
+        isAdmin:boolean;
+        postAmount:number;
+        refreshToken:string;
+    },
+    params:{
+        userId:string
+    }
+}
+interface UserResponse extends Response<any,Record<string,any>>{
+    status(code:number): this,
+    json(data:any):this
+}
 const userController:any = {
+    //user does
     getUser: async(req:Request,res:Response)=>{
         const {userId} = req.params
         try {
@@ -42,6 +62,57 @@ const userController:any = {
             res.status(500).json({message:error})
         }
     },
+    //admin does
+    getAllUser: async(req:UserRequest,res:UserResponse) =>{
+        try {
+            const users = await db.User.findAll()
+            res.status(200).json(users)
+        } catch (error) {
+            res.status(500).json({message:error})
+        }
+    },
+    getOneUser: async(req:UserRequest,res:UserResponse)=>{
+        const {userId} = req.params
+        try {
+            const user = await db.User.findByPk(userId)
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(500).json({message:error})
+        }
+    },
+    updateUserByAdmin: async(req:UserRequest,res:UserResponse)=>{
+        const {userId} = req.params
+        const {name,email,serviceId,isAdmin,postAmount} = req.body
+        try {
+            const user = await db.User.update({
+                name,
+                email,
+                serviceId,
+                isAdmin,
+                postAmount,
+            },{
+                where:{
+                    id:userId
+                }
+            })
+            res.status(200).json({message:"User has been updated"})
+        } catch (error) {
+            res.status(500).json({message:error})
+        }
+    },
+    deleteUserByAdmin: async(req:UserRequest,res:UserResponse)=>{
+        const {userId} = req.params
+        try {
+            await db.User.destroy({
+                where:{
+                    id:userId
+                }
+            })
+            res.status(200).json({message:"User has been deleted"})
+        } catch (error) {
+            res.status(500).json({message:error})
+        }
+    }
 }
 
 export default userController;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,7 +3,14 @@ import userController from '../controllers/user.controller';
 import middlewareController from '../controllers/middleware.controller';
 const router:express.Router = express.Router();
 //user does
-router.get('/:userId',middlewareController.verifyTokenAndAuthorize,userController.getUser)
-router.put('/update/:userId',middlewareController.verifyTokenAndAuthorize,userController.updateUser)
-router.delete('/delete/:userId',middlewareController.verifyTokenAndAuthorize,userController.deleteUser)
+router.get('/get/:userId',middlewareController.verifyTokenAndAuthorize,userController.getUser)
+router.put('/update-user/:userId',middlewareController.verifyTokenAndAuthorize,userController.updateUser)
+router.delete('/delete-user/:userId',middlewareController.verifyTokenAndAuthorize,userController.deleteUser)
+
+//admin does
+router.get('/',middlewareController.verifyTokenAndAdmin,userController.getAllUser)
+router.get('/:userId',middlewareController.verifyTokenAndAdmin,userController.getOneUser)
+router.put('/update/:userId',middlewareController.verifyTokenAndAdmin,userController.updatUserByAdmin)
+router.delete('/delete/:userId',middlewareController.verifyTokenAndAdmin,userController.deleteUserByAdmin)
+
 export default router;
