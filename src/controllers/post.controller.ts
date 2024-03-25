f4195e00ef1bd465e7c4e352837259bbdc024e3a
trieu <lequoctrieu618@gmail.com>
2024-03-25 13:27:53 +0700

    update cud for user post

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -35,7 +35,7 @@ const postController = {
     const { title, description, roomType, price, address, acreage, roomImage } =
       req.body;
     try {
-      const post:IPost = await db.Post.create({
+      const post: IPost = await db.Post.create({
         title,
         description,
         roomType,
@@ -59,18 +59,18 @@ const postController = {
   },
   getAllPost: async (req: PostRequest, res: PostResponse) => {
     try {
-      const posts:IPost = await db.Post.findAll({
+      const posts: IPost = await db.Post.findAll({
         include: [
           {
             model: db.User,
             attributes: ["id", "username", "email", "phone"],
             include: [
-                {
-                    model: db.Service, 
-                    attributes: ["id", "name", "price"],
-                    order:[["price", "DESC"]]
-                },
-            ]
+              {
+                model: db.Service,
+                attributes: ["id", "name", "price"],
+                order: [["price", "DESC"]],
+              },
+            ],
           },
           {
             model: db.ImageRoom,
@@ -87,31 +87,81 @@ const postController = {
           "acreage",
           "createdAt",
         ],
-        order: [
-          ["createdAt", "ASC"],
-        ],
+        order: [["createdAt", "ASC"]],
       });
       res.status(200).json(posts);
     } catch (error) {
       res.status(500).json({ message: error });
     }
   },
-  getOnePost: async(req: PostRequest,res:PostResponse)=>{
-    const {postId} = req.params
+  getOnePost: async (req: PostRequest, res: PostResponse) => {
+    const { postId } = req.params;
     try {
-      const post:IPost = await db.Post.findOne({
-        where:{id:postId},
-        attributes:['id','title','description','roomType','price','address','acreage'],
-        include:[
-          {model:db.ImageRoom,attributes:['id','imageUrl']},
-          {model:db.User,attributes:['id','username','email','phone']}
-        ]
-      })
-      res.status(200).json(post)
+      const post: IPost = await db.Post.findOne({
+        where: { id: postId },
+        attributes: [
+          "id",
+          "title",
+          "description",
+          "roomType",
+          "price",
+          "address",
+          "acreage",
+        ],
+        include: [
+          { model: db.ImageRoom, attributes: ["id", "imageUrl"] },
+          { model: db.User, attributes: ["id", "username", "email", "phone"] },
+        ],
+      });
+      res.status(200).json(post);
     } catch (error) {
-      res.status(500).json({message:error})
+      res.status(500).json({ message: error });
     }
-  }
+  },
+  //admin
+  deletePost: async (req: PostRequest, res: PostResponse) => {
+    const { postId } = req.params;
+    try {
+      await db.Post.destroy({
+        where: { id: postId },
+      });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  },
+  //user
+  deletePostByUser: async (req: PostRequest, res: PostResponse) => {
+    const { postId } = req.params;
+    try {
+      await db.Post.destroy({
+        where: { id: postId },
+      });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  },
+  updatePost: async (req: PostRequest, res: PostResponse) => {
+    const { postId } = req.params;
+    const { title, description, roomType, price, address, acreage } = req.body;
+    try {
+      const updatePost = await db.Post.update(
+        {
+          where: { id: postId },
+        },
+        {
+          title,
+          description,
+          roomType,
+          price,
+          address,
+          acreage,
+        }
+      );
+      res.status(200).json({ message: "Post updated successfully" });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  },
 };
 
 export default postController;
--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -2,8 +2,11 @@ import middlewareController from "../controllers/middleware.controller";
 import postController from "../controllers/post.controller";
 import express from "express";
 const router = express.Router()
-router.post('/create/:userId',middlewareController.verifyTokenAndAuthorize,postController.createPost)
-
+router.delete('/delete/:postId/:userId',middlewareController.verifyTokenAndAdmin,postController.deletePost)
+//user does
+router.post('/create-post/:userId',middlewareController.verifyTokenAndAuthorize,postController.createPost)
+router.delete('/delete-post/:postId/:userId',middlewareController.verifyTokenAndAuthorize,postController.deletePost)
+router.put('/update-post/:postId/:userId',middlewareController.verifyTokenAndAuthorize,postController.updatePost)
 //all user does
 router.get('/',postController.getAllPost)
 router.get('/:postId',postController.getOnePost)
